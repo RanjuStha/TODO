2998ad40d6bca51d22f44175d56810d8eed8dce2
ranju_bentray <Ranju@bentraytech.com>
2021-04-03 21:39:20 +0545

    basic updates

--- ToDoProject/Helper/helperClass.swift
+++ ToDoProject/Helper/helperClass.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+func displayAlertMessage(userMessage:String, myView:UIViewController)
+{
+    let myAlert=UIAlertController(title:"Sync Simple",message:userMessage, preferredStyle:UIAlertController.Style.alert);
+    let okAction=UIAlertAction(title:"Ok", style:UIAlertAction.Style.default, handler:nil)
+    myAlert.addAction(okAction)
+    myView.present(myAlert,animated:true, completion: nil)
+}
